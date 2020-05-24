6771cd33983a68097852b7c7a102671e0e57f006
Dario Limongi <dario.limongi@gmail.com>
2020-05-24 16:30:10 +0200

    Show message about premium account and supported browsers

--- src/components/spotify/SpotifyLogin.jsx
+++ src/components/spotify/SpotifyLogin.jsx
@@ -4,14 +4,19 @@ import { getAuthorizationUrl } from "./Utils.js";
 
 const SpotifyLogin = ({ handleLoginClick }) => {
   return (
-    <div className="login-wrapper">
-      <a
-        className="btn-spotify"
-        href={getAuthorizationUrl()}
-        onClick={handleLoginClick}
-      >
-        Login to Spotify
-      </a>
+    <div className="spotify-wrapper">
+      <div className="white-text-centered">
+        <p>In order to play Spotify songs you need a Premium account.</p>
+        <p>Supported browsers: Chrome / Firefox / Edge</p>
+        <br></br>
+        <a
+          className="btn-spotify"
+          href={getAuthorizationUrl()}
+          onClick={handleLoginClick}
+        >
+          Login to Spotify
+        </a>
+      </div>
     </div>
   );
 };
